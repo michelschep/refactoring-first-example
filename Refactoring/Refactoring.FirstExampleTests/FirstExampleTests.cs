dfea9d9c587dd0b0e0664a806f8da823756448d5
Michel Schep <michelschep@gmail.com>
2019-01-26 16:53:31 +0100

    Add basic solution with unit test

---
+++ Refactoring/Refactoring.FirstExampleTests/FirstExampleTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Refactoring.FirstExampleTests
+{
+    public class FirstExampleTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
